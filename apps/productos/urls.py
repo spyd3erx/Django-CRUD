89c23d56b788021f53db75bc4c9563c52d0625c7
Juan Delgado <juanddelgadoguerra@gmail.com>
2025-06-06 15:32:43 -0500

    feat(productos): agregar creación de productos
    - Template: products/add_product.html
    
    - View: add_new_product

--- apps/productos/urls.py
+++ apps/productos/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name="productos"
 urlpatterns = [
     path("productos/", views.index, name="index"),
+    path("add_product/", views.add_new_product, name="add_product"),
 ]
--- apps/productos/views.py
+++ apps/productos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Product
 
 
@@ -7,3 +8,16 @@ def index(request):
     productos = Product.objects.all()
     context = {"productos": productos, "categorias": Product.Category.choices}
     return render(request, "productos/list_products.html", context)
+
+
+def add_new_product(request):
+    product_name = request.POST["product_name"]
+    product_price = request.POST["product_price"]
+    product_category = request.POST["product_category"]
+    Product.objects.create(
+        product_name=product_name,
+        valor_unitario=product_price,
+        categoria=product_category
+    )
+    messages.success(request, "producto agregado")
+    return redirect("/productos/")
